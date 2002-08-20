33df59afe9f8f54840432af4e9f4b3a558c63e8e
fimath <>
2002-08-20 16:30:15 +0000

    [project @ 216] First completed version.

--- tools/results.c
+++ tools/results.c
@@ -14,18 +14,34 @@
 #include <time.h>
 #include <common.h>
 
+char *transaction_name[TRANSACTION_MAX] =
+	{ "delivery    ",
+	  "new-order   ",
+	  "order-status",
+	  "payment     ",
+	  "stock-level "
+	};
+
 int main(int argc, char *argv[])
 {
+	int i;
 	FILE *log_mix;
 	FILE *log_tps;
-	int sample_length;
-	int total_transaction_count;
-	double response_time, total_response_time;
+	int sample_length = 30;
+	int total_transaction_count = 0;
+	int total_new_order_count = 0;
+	int current_transaction_count = 0;
+	double response_time, total_response_time = 0;
 	time_t start_time = -1;
-	time_t current_time;
+	time_t previous_time, current_time;
 	char transaction;
 	char marker[64];
 	int tid;
+	int elapsed_time;
+	double tps;
+
+	int transaction_count[TRANSACTION_MAX] = { 0, 0, 0, 0, 0 };
+	double transaction_response_time[TRANSACTION_MAX] = { 0, 0, 0, 0, 0 };
 
 	if (argc < 3)
 	{
@@ -57,13 +73,14 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	while (fscanf(log_mix, "%d,%c,%f,%d", &current_time, &transaction,
+	while (fscanf(log_mix, "%d,%c,%lf,%d", &current_time, &transaction,
 		&response_time, &tid) == 4)
 	{
 		/* Note when the rampup has ended. */
 		if (start_time == -1)
 		{
 			start_time = current_time;
+			previous_time = current_time;
 		}
 
 		total_response_time += response_time;
@@ -71,25 +88,70 @@ int main(int argc, char *argv[])
 		
 		if (transaction == 'n')
 		{
+			++transaction_count[NEW_ORDER];
+			transaction_response_time[NEW_ORDER] += response_time;
+			++total_new_order_count;
 		}
 		else if (transaction == 'p')
 		{
+			++transaction_count[PAYMENT];
+			transaction_response_time[PAYMENT] += response_time;
 		}
 		else if (transaction == 'o')
 		{
+			++transaction_count[ORDER_STATUS];
+			transaction_response_time[ORDER_STATUS] += response_time;
 		}
 		else if (transaction == 'd')
 		{
+			++transaction_count[DELIVERY];
+			transaction_response_time[DELIVERY] += response_time;
 		}
 		else if (transaction == 's')
 		{
+			++transaction_count[STOCK_LEVEL];
+			transaction_response_time[STOCK_LEVEL] += response_time;
 		}
 		else
 		{
-			printf("unknown transaction\n");
-			return 4;
+			printf("unknown transaction, continuing\n");
+			continue;
+		}
+
+		/* Output data to csv file for charting transaction per second. */
+		if (current_time <= previous_time + sample_length)
+		{
+			++current_transaction_count;
+		}
+		else
+		{
+			fprintf(log_tps, "%d,%f\n", elapsed_time,
+				(double) current_transaction_count / 30.0);
+				elapsed_time += sample_length;
+
+			previous_time = current_time;
+			current_transaction_count = 1;
 		}
 	}
+	fclose(log_mix);
+	fclose(log_tps);
+
+	/* Calculate the actual mix of transactions. */
+	printf("transaction\t%\tavg response time (s)\n");
+	for (i = 0; i < TRANSACTION_MAX; i++)
+	{
+		printf("%s\t%2.2f\t%0.3f\n", transaction_name[i],
+			(double) transaction_count[i] / (double) total_transaction_count * 100.0,
+			transaction_response_time[i] / (double) transaction_count[i]);
+	}
+
+	/* Calculated the number of transactions per second. */
+	tps = (double) total_new_order_count / difftime(current_time, start_time);
+	printf("\n%0.2f bogotransactions (type 2) per second\n", tps);
+	printf("%0.1f minute duration\n",
+		difftime(current_time, start_time) / 60.0);
+	printf("%d total bogotransactions (type 2)\n", total_new_order_count);
+	printf("\n");
 
 	return 0;
 }
