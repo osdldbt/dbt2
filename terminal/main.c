956407065179c29d9ad902e207852ee64188d19c
fimath <>
2002-08-21 20:24:02 +0000

    [project @ 223] Added parameters so that the range of warehouses to run against can be set, as well as the keying time and thinking time.

--- terminal/main.c
+++ terminal/main.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 
 	if (parse_arguments(argc, argv) != OK)
 	{
-		printf("usage: %s -d <address> -w # -u # -l # [-p #] [-c #] [-i #] [-o #] [-s #] [-n #] [-q %] [-r %] [-e %] [-t %]\n",
+		printf("usage: %s -d <address> -wmin # -wmax # -l # [-w #] [-p #] [-c #] [-i #] [-o #] [-s #] [-n #] [-q %] [-r %] [-e %] [-t %]\n",
 			argv[0]);
 		printf("\n");
 		printf("-d <address>\n");
@@ -34,13 +34,15 @@ int main(int argc, char *argv[])
 		printf("-p #\n");
 		printf("\tclient port, default %d\n", CLIENT_PORT);
 		printf("\n");
-		printf("-u #\n");
-		printf("\tthe number of terminals to emulate\n");
 		printf("-l #\n");
 		printf("\tthe duration of the run in seconds\n");
 		printf("\n");
+		printf("-wmin #\n");
+		printf("\tlower warehouse id\n");
+		printf("-wmax #\n");
+		printf("\tupper warehouse id\n");
 		printf("-w #\n");
-		printf("\tnumber of warehouses\n");
+		printf("\twarehouse cardinality, default 1\n");
 		printf("-c #\n");
 		printf("\tcustomer cardinality, default %d\n", CUSTOMER_CARDINALITY);
 		printf("-i #\n");
@@ -64,6 +66,43 @@ int main(int argc, char *argv[])
 		printf("-t %\n");
 		printf("\tmix percentage of Stock-Level transaction, default %0.2f\n",
 			MIX_STOCK_LEVEL);
+		printf("\n");
+		printf("-ktd #\n");
+		printf("\tdelivery keying time, default %d s", KEY_TIME_DELIVERY);
+		printf("-ktn #\n");
+		printf("\tnew-order keying time, default %d s", KEY_TIME_NEW_ORDER);
+		printf("-kto #\n");
+		printf("\torder-status keying time, default %d s",
+			KEY_TIME_ORDER_STATUS);
+		printf("-ktp #\n");
+		printf("\tpayment keying time, default %d s", KEY_TIME_PAYMENT);
+		printf("-kts #\n");
+		printf("\tstock-level keying time, default %d s", KEY_TIME_STOCK_LEVEL);
+		printf("-ttd #\n");
+		printf("\tdelivery thinking time, default %d ms", THINK_TIME_DELIVERY);
+		printf("-ttn #\n");
+		printf("\tnew-order thinking time, default %d ms",
+			THINK_TIME_NEW_ORDER);
+		printf("-tto #\n");
+		printf("\torder-status thinking time, default %d ms",
+			THINK_TIME_ORDER_STATUS);
+		printf("-ttp #\n");
+		printf("\tpayment thinking time, default %d ms", THINK_TIME_PAYMENT);
+		printf("-tts #\n");
+		printf("\tstock-level thinking time, default %d ms",
+			THINK_TIME_STOCK_LEVEL);
+		return 1;
+	}
+
+	/* Sanity check on the parameters. */
+	if (w_id_min > w_id_max)
+	{
+		printf("wmin cannot be larger than wmax\n");
+		return 1;
+	}
+	if (w_id_max > table_cardinality.warehouses)
+	{
+		printf("wmax cannot be larger than w\n");
 		return 1;
 	}
 
@@ -110,7 +149,26 @@ int main(int argc, char *argv[])
 		transaction_mix.stock_level_threshold);
 	printf("\n");
 
-	start_driver(terminals);
+	/* Double check the keying time. */
+	printf("delivery keying time %d s\n", key_time.delivery);
+	printf("new_order keying time %d s\n", key_time.new_order);
+	printf("order-status keying time %d s\n", key_time.order_status);
+	printf("payment keying time %d s\n", key_time.payment);
+	printf("stock-level keying time %d s\n", key_time.stock_level);
+	printf("\n");
+
+	/* Double check the thinking time. */
+
+	printf("delivery thinking time %d ms\n", think_time.delivery);
+	printf("new_order thinking time %d ms\n", think_time.new_order);
+	printf("order-status thinking time %d ms\n", think_time.order_status);
+	printf("payment thinking time %d ms\n", think_time.payment);
+	printf("stock-level thinking time %d ms\n", think_time.stock_level);
+
+	printf("w_id range %d to %d\n", w_id_min, w_id_max);
+	printf("\n");
+
+	start_driver();
 
 	return 0;
 }
@@ -118,79 +176,125 @@ int main(int argc, char *argv[])
 int parse_arguments(int argc, char *argv[])
 {
 	int i;
+	char *flag;
 
-	if (argc < 6)
+	if (argc < 9)
 	{
 		return ERROR;
 	}
 
 	for (i = 1; i < argc; i += 2)
 	{
-		if (strlen(argv[i]) != 2)
+		if (strlen(argv[i]) < 2)
 		{
 			printf("invalid flag: %s\n", argv[i]);
-			return ERROR;
+			exit(1);
 		}
-		if (argv[i][1] == 'd')
+		flag = argv[i] + 1;
+		if (strcmp(flag, "d") == 0)
 		{
 			set_client_hostname(argv[i + 1]);
 		}
-		else if (argv[i][1] == 'p')
+		else if (strcmp(flag, "p") == 0)
 		{
 			set_client_port(atoi(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'u')
-		{
-			set_terminals(atoi(argv[i + 1]));
-		}
-		else if (argv[i][1] == 'l')
+		else if (strcmp(flag, "l") == 0)
 		{
 			set_duration(atoi(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'w')
+		else if (strcmp(flag, "w") == 0)
 		{
 			set_table_cardinality(TABLE_WAREHOUSE, atoi(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'c')
+		else if (strcmp(flag, "c") == 0)
 		{
 			set_table_cardinality(TABLE_CUSTOMER, atoi(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'i')
+		else if (strcmp(flag, "i") == 0)
 		{
 			set_table_cardinality(TABLE_ITEM, atoi(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'o')
+		else if (strcmp(flag, "o") == 0)
 		{
 			set_table_cardinality(TABLE_ORDER, atoi(argv[i + 1]));
 		}
-		else if (argv[i][1] == 's')
+		else if (strcmp(flag, "s") == 0)
 		{
 			set_table_cardinality(TABLE_STOCK, atoi(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'n')
+		else if (strcmp(flag, "n") == 0)
 		{
 			set_table_cardinality(TABLE_NEW_ORDER, atoi(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'q')
+		else if (strcmp(flag, "q") == 0)
 		{
 			set_transaction_mix(PAYMENT, atof(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'r')
+		else if (strcmp(flag, "r") == 0)
 		{
 			set_transaction_mix(ORDER_STATUS, atof(argv[i + 1]));
 		}
-		else if (argv[i][1] == 'e')
+		else if (strcmp(flag, "e") == 0)
 		{
 			set_transaction_mix(DELIVERY, atof(argv[i + 1]));
 		}
-		else if (argv[i][1] == 't')
+		else if (strcmp(flag, "t") == 0)
 		{
 			set_transaction_mix(STOCK_LEVEL, atof(argv[i + 1]));
+		}
+		else if (strcmp(flag, "wmin") == 0)
+		{
+			w_id_min = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "wmax") == 0)
+		{
+			w_id_max = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "ktd") == 0)
+		{
+			key_time.delivery = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "ktn") == 0)
+		{
+			key_time.new_order = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "kto") == 0)
+		{
+			key_time.order_status = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "ktp") == 0)
+		{
+			key_time.payment = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "kts") == 0)
+		{
+			key_time.stock_level = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "ttd") == 0)
+		{
+			think_time.delivery = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "ttn") == 0)
+		{
+			think_time.new_order = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "tto") == 0)
+		{
+			think_time.order_status = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "ttp") == 0)
+		{
+			think_time.payment = atoi(argv[i + 1]);
+		}
+		else if (strcmp(flag, "tts") == 0)
+		{
+			think_time.stock_level = atoi(argv[i + 1]);
 		}
 		else
 		{
 			printf("invalid flag: %s\n", argv[i]);
-			return ERROR;
+			exit(1);
 		}
 	}
 
