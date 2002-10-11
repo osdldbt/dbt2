22d9e75d1504c2b92ade9749459f5153482cef28
fimath <>
2002-10-11 16:28:36 +0000

    [project @ 313] Fixed a typo in a create index statement.

--- scripts/sapdb/create_indexes.sh
+++ scripts/sapdb/create_indexes.sh
@@ -3,7 +3,7 @@ DBNAME=DBT2
 _o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt 2>&1
 sql_execute create unique index i_warehouse on warehouse (w_id asc)
 sql_execute create unique index i_district on district (d_w_id, d_id)
-sql_execute create unique index i_customer on customer (c_w_id, c_d_id, c_id)
+sql_execute create unique index i_customer on customer (c_w_id, c_d_id, o_c_id)
 sql_execute create unique index i_new_order on new_order (no_w_id, no_d_id, no_o_id)
 sql_execute create unique index i_orders on orders (o_w_id, o_d_id, o_id)
 sql_execute create unique index i_orders2 on orders (o_w_id, o_d_id, c_id)
