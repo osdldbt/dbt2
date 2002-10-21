26f20335f2c5bb197ecbd5c2a759db9d94a90c39
fimath <>
2002-10-21 16:21:02 +0000

    [project @ 327] Removed the unique indexes for columns that are now being defined as primary keys.

--- scripts/sapdb/create_indexes.sh
+++ scripts/sapdb/create_indexes.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 DBNAME=DBT2
 _o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt 2>&1
-sql_execute create unique index i_warehouse on warehouse (w_id asc)
-sql_execute create unique index i_district on district (d_w_id, d_id)
-sql_execute create unique index i_customer on customer (c_w_id, c_d_id, o_c_id)
-sql_execute create unique index i_new_order on new_order (no_w_id, no_d_id, no_o_id)
-sql_execute create unique index i_orders on orders (o_w_id, o_d_id, o_id)
-sql_execute create unique index i_orders2 on orders (o_w_id, o_d_id, c_id)
-sql_execute create unique index i_order_line on order_line (ol_w_id, ol_d_id, ol_o_id, ol_number)
-sql_execute create unique index i_item on item (i_id)
-sql_execute create unique index i_stock on stock(s_w_id, s_i_id)
+sql_execute create unique index i_orders2 on orders (o_w_id, o_d_id, o_c_id)
 EOF`
 echo $_o
