1d389aaf2d90bf487d3670e8b9661af4e3813383
fimath <>
2002-11-20 18:56:57 +0000

    [project @ 390] The stored procedure stock_level2 for SAP DB doesn't exist anymore, so no need to drop it.

--- scripts/sapdb/drop_dbproc.sh
+++ scripts/sapdb/drop_dbproc.sh
@@ -13,4 +13,3 @@ SID=DBT2
 /opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc delivery_2
 
 /opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc stock_level
-/opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc stock_level_2
