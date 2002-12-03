c5e3d1f49c558ea5a01de65989743cc26ca7ae67
fimath <>
2002-12-03 18:21:09 +0000

    [project @ 409] Added statement so that the stock table statistics are updated.

--- scripts/sapdb/update_stats.sh
+++ scripts/sapdb/update_stats.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-/opt/sapdb/indep_prog/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics warehouse
-/opt/sapdb/indep_prog/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics district
-/opt/sapdb/indep_prog/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics customer
-/opt/sapdb/indep_prog/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics history
-/opt/sapdb/indep_prog/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics new_order
-/opt/sapdb/indep_prog/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics orders
-/opt/sapdb/indep_prog/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics order_line
-/opt/sapdb/indep_prog/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics item
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics warehouse
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics district
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics customer
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics history
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics new_order
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics orders
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics order_line
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics item
+/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics stock
