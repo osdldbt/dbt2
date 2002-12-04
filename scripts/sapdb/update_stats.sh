9f3c0fec184a30cf49145d436a99f399d9250c2e
fimath <>
2002-12-04 23:11:16 +0000

    [project @ 414] Fixed a path problem.

--- scripts/sapdb/update_stats.sh
+++ scripts/sapdb/update_stats.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics warehouse
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics district
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics customer
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics history
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics new_order
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics orders
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics order_line
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics item
-/opt/sapdb/depend74/bin/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics stock
+
+SAPDBBINDIR=/opt/sapdb/depend/bin
+
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics warehouse
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics district
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics customer
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics history
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics new_order
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics orders
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics order_line
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics item
+$SAPDBBINDIR/dbmcli -d DBT2 -u dba,dba -uSQL dbt,dbt sql_execute update statistics stock
