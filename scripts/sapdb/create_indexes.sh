d8e08c64cca7897901b2ca5f44a215074da6504e
fimath <>
2002-12-12 22:56:05 +0000

    [project @ 428] Renamed i_orders2 to i_orders and created a new index on the customer table.

--- scripts/sapdb/create_indexes.sh
+++ scripts/sapdb/create_indexes.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
 DBNAME=DBT2
-_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt 2>&1
-sql_execute create unique index i_orders2 on orders (o_w_id, o_d_id, o_c_id)
-EOF`
-echo $_o
+SAPDBBINDIR=/opt/sapdb/depend/bin
+export PATH=$PATH:$SAPDBBINDIR
+
+set -x
+
+dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt -c "sql_execute create unique index i_orders on orders (o_w_id, o_d_id, o_c_id)"
+
+dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt -c "sql_execute create index i_custom
+er on customer (c_w_id, c_d_id, c_last, c_first, c_id)"
