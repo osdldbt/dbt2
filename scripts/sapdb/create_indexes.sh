45f1354ee5f684b3aacd57c8f82752b34e45fd37
fimath <>
2002-12-12 22:59:29 +0000

    [project @ 429] Fixed a typo by removing an extra carriage return.

--- scripts/sapdb/create_indexes.sh
+++ scripts/sapdb/create_indexes.sh
@@ -8,5 +8,4 @@ set -x
 
 dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt -c "sql_execute create unique index i_orders on orders (o_w_id, o_d_id, o_c_id)"
 
-dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt -c "sql_execute create index i_custom
-er on customer (c_w_id, c_d_id, c_last, c_first, c_id)"
+dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt -c "sql_execute create index i_customer on customer (c_w_id, c_d_id, c_last, c_first, c_id)"
