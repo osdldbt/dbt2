7ed65ca934ab6ef6b6d478cc45ecf44fac908ade
fimath <>
2002-12-13 21:52:23 +0000

    [project @ 439] Corrected the indexed on orders which prevented inserts into the table.

--- scripts/sapdb/create_indexes.sh
+++ scripts/sapdb/create_indexes.sh
@@ -6,6 +6,6 @@ export PATH=$PATH:$SAPDBBINDIR
 
 set -x
 
-dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt -c "sql_execute create unique index i_orders on orders (o_w_id, o_d_id, o_c_id)"
+dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt -c "sql_execute create unique index i_orders on orders (o_w_id, o_d_id, o_c_id, o_id)"
 
 dbmcli -d $DBNAME -u dba,dba -uSQL dbt,dbt -c "sql_execute create index i_customer on customer (c_w_id, c_d_id, c_last, c_first, c_id)"
