2718101ff285e452c126d04b27f7cfa5ea7c7fb0
fimath <>
2002-12-19 18:39:17 +0000

    [project @ 475] Revised the script so it may be easier to update in the future.

--- scripts/sapdb/drop_dbproc.sh
+++ scripts/sapdb/drop_dbproc.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 SID=DBT2
+SAPBINDIR=/opt/sapdb/depend/bin/
+export PATH=$PATH:$SAPBINDIR
 
-/opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc new_order
-/opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc new_order_2
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc new_order
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc new_order_2
 
-/opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc payment
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc payment
 
-/opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc order_status
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc order_status
 
-/opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc delivery
-/opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc delivery_2
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc delivery
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc delivery_2
 
-/opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc stock_level
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt sql_execute drop dbproc stock_level
