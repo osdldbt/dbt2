789b97a2a8a3670166ec000ba5ae79df5bab057d
fimath <>
2002-12-19 18:38:00 +0000

    [project @ 474] Revised the script so that it might be easier to update in the future.

--- scripts/sapdb/load_dbproc.sh
+++ scripts/sapdb/load_dbproc.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 SID=DBT2
+SAPDBBINDIR=/opt/sapdb/depend/bin
+export PATH=$PATH:$SAPDBBINDIR
 
-/opt/sapdb/depend/bin/repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/new_order_2.sql
-/opt/sapdb/depend/bin/repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/new_order.sql
+repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/new_order_2.sql
+repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/new_order.sql
 
-/opt/sapdb/depend/bin/repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/payment.sql
+repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/payment.sql
 
-/opt/sapdb/depend/bin/repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/order_status.sql
+repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/order_status.sql
 
-/opt/sapdb/depend/bin/repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/delivery_2.sql
-/opt/sapdb/depend/bin/repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/delivery.sql
+repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/delivery_2.sql
+repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/delivery.sql
 
-/opt/sapdb/depend/bin/repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/stock_level_2.sql
-/opt/sapdb/depend/bin/repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/stock_level.sql
+repmcli -d $SID -u dbt,dbt -b ../../storedproc/sapdb/stock_level.sql
