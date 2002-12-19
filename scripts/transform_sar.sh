461185ccf094d1d8c5b00083323a2910efe8d356
fimath <>
2002-12-19 18:11:04 +0000

    [project @ 469] Call gnuplot to generate graphs.

--- scripts/transform_sar.sh
+++ scripts/transform_sar.sh
@@ -9,3 +9,6 @@ sar -f $SAROUTPUT -u | grep all | grep -v Average | awk '{ print (NR - 1) * '$SA
 sar -f $SAROUTPUT -u | grep all | grep -v Average | awk '{ print (NR - 1) * '$SAMPLE_LENGTH', $4 }' > $OUTPUTDIR/cpu_user.out
 sar -f $SAROUTPUT -u | grep all | grep -v Average | awk '{ print (NR - 1) * '$SAMPLE_LENGTH', $5 }' > $OUTPUTDIR/cpu_nice.out
 sar -f $SAROUTPUT -u | grep all | grep -v Average | awk '{ print (NR - 1) * '$SAMPLE_LENGTH', $6 }' > $OUTPUTDIR/cpu_system.out
+cd $OUTPUTDIR
+gnuplot ../../cpu.input
+cd -
