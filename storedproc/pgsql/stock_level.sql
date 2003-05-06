b9bb9734e7257e6d0232a39877d9c073d3dd2d68
fimath <>
2003-05-06 17:00:01 +0000

    [project @ 978] try to make it more readable.

--- storedproc/pgsql/stock_level.sql
+++ storedproc/pgsql/stock_level.sql
@@ -2,7 +2,7 @@
  * This file is released under the terms of the Artistic License.  Please see
  * the file LICENSE, included in this package, for details.
  *
- * Copyright (C) 2002 Mark Wong & Open Source Development Lab, Inc.
+ * Copyright (C) 2003 Mark Wong & Open Source Development Lab, Inc.
  *
  * Based on TPC-C Standard Specification Revision 5.0 Clause 2.8.2.
  */
@@ -11,7 +11,9 @@ DECLARE
 	in_w_id ALIAS FOR $1;
 	in_d_id ALIAS FOR $2;
 	in_threshold ALIAS FOR $3;
+
 	tmp_d_next_o_id INTEGER;
+
 	low_stock INTEGER;
 BEGIN
 	SELECT d_next_o_id
@@ -19,6 +21,7 @@ BEGIN
 	FROM district
 	WHERE d_w_id = in_w_id
 	AND d_id = in_d_id;
+
 	SELECT count(*)
 	INTO low_stock
 	FROM order_line, stock, district
@@ -31,6 +34,7 @@ BEGIN
 	  AND s_quantity < in_threshold
 	  AND ol_o_id BETWEEN (tmp_d_next_o_id - 20)
 	                  AND (tmp_d_next_o_id -1);
+
 	RETURN low_stock;
 END;
 ' LANGUAGE 'plpgsql';
