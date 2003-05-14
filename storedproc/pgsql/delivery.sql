7c0b4badad963db1e8cd914b585a29ee5f5982a5
fimath <>
2003-05-14 18:07:20 +0000

    [project @ 989] didn't need to pass d_id into the function.  use the value from the loop.

--- storedproc/pgsql/delivery.sql
+++ storedproc/pgsql/delivery.sql
@@ -6,11 +6,10 @@
  *
  * Based on TPC-C Standard Specification Revision 5.0 Clause 2.8.2.
  */
-CREATE OR REPLACE FUNCTION delivery (INTEGER, INTEGER, INTEGER) AS '
+CREATE OR REPLACE FUNCTION delivery (INTEGER, INTEGER) AS '
 DECLARE
 	in_w_id ALIAS FOR $1;
 	in_o_carrier_id ALIAS FOR $2;
-	in_d_id ALIAS FOR $3;
 
 	out_c_id INTEGER;
 	out_ol_amount NUMERIC;
@@ -25,46 +24,46 @@ BEGIN
 		INTO tmp_o_id
 		FROM new_order
 		WHERE no_w_id = in_w_id
-		  AND no_d_id = in_d_id;
+		  AND no_d_id = tmp_d_id;
 
 		IF tmp_o_id > 0 THEN
 			DELETE FROM new_order
 			WHERE no_o_id = tmp_o_id
 			  AND no_w_id = in_w_id
-			  AND no_d_id = in_d_id;
+			  AND no_d_id = tmp_d_id;
 
 			SELECT o_c_id
 			INTO out_c_id
 			FROM orders
 			WHERE o_id = tmp_o_id
 			  AND o_w_id = in_w_id
-			  AND o_d_id = in_d_id;
+			  AND o_d_id = tmp_d_id;
 
 			UPDATE orders
 			SET o_carrier_id = in_o_carrier_id
 			WHERE o_id = tmp_o_id
 			  AND o_w_id = in_w_id
-			  AND o_d_id = in_d_id;
+			  AND o_d_id = tmp_d_id;
 
 			UPDATE order_line
 			SET ol_delivery_d = current_timestamp
 			WHERE ol_o_id = tmp_o_id
 			  AND ol_w_id = in_w_id
-			  AND ol_d_id = in_d_id;
+			  AND ol_d_id = tmp_d_id;
 
 			SELECT SUM(ol_amount * ol_quantity)
 			INTO out_ol_amount
 			FROM order_line
 			WHERE ol_o_id = tmp_o_id
 			  AND ol_w_id = in_w_id
-			  AND ol_d_id = in_d_id;
+			  AND ol_d_id = tmp_d_id;
 
 			UPDATE customer
 			SET c_delivery_cnt = c_delivery_cnt + 1,
 			    c_balance = c_balance + out_ol_amount
 			WHERE c_id = out_c_id
 			  AND c_w_id = in_w_id
-			  AND c_d_id = in_d_id;
+			  AND c_d_id = tmp_d_id;
 		END IF;
 	END LOOP;
 END;
