9df0cfe15678b015eccf2a660304ee790557d980
fimath <>
2003-06-03 17:43:16 +0000

    [project @ 1067] fixed a typo, and updated the error logging reporting

--- interfaces/libpq/libpq_payment.c
+++ interfaces/libpq/libpq_payment.c
@@ -21,20 +21,20 @@ int execute_payment(struct db_context_t *dbc, struct payment_t *data)
 	/* Start a transaction block. */
 	res = PQexec(dbc->conn, "BEGIN");
 	if (!res || PQresultStatus(res) != PGRES_COMMAND_OK) {
-		LOG_ERROR_MESSAGE("BEGIN command failed.\n");
+		LOG_ERROR_MESSAGE("%s", PQerrorMessage(dbc->conn));
 		PQclear(res);
 		return ERROR;
 	}
 	PQclear(res);
 
 	/* Create the query and execute it. */
-	sprintf(stmt, "SELECT payment(%d, %d, %d, %d %d, '%s', %f)",
+	sprintf(stmt, "SELECT payment(%d, %d, %d, %d, %d, '%s', %f)",
 		data->w_id, data->d_id, data->c_id, data->c_w_id, data->c_w_id,
 		data->c_last, data->h_amount);
 	res = PQexec(dbc->conn, stmt);
 	if (!res || (PQresultStatus(res) != PGRES_COMMAND_OK &&
 		PQresultStatus(res) != PGRES_TUPLES_OK)) {
-		LOG_ERROR_MESSAGE("%s", PQerrorMessage(dbc->conn));
+		LOG_ERROR_MESSAGE("%s\n%s", stmt, PQerrorMessage(dbc->conn));
 		PQclear(res);
 		return ERROR;
 	}
