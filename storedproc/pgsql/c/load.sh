88cf060d874bc89904de64c4a3e55de7c2135c02
fimath <>
2005-03-04 03:17:17 +0000

    [project @ 1205] Removing old files.

--- storedproc/pgsql/c/load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-psql -d dbt2 -f delivery.sql
-psql -d dbt2 -f new_order.sql
-psql -d dbt2 -f order_status.sql
-psql -d dbt2 -f payment2.sql
-psql -d dbt2 -f payment.sql
-psql -d dbt2 -f stock_level.sql
--- tools/load.sh
+++
@@ -1,6 +0,0 @@
-psql -d dbt2 -f delivery.sql
-psql -d dbt2 -f new_order_2.sql
-psql -d dbt2 -f new_order.sql
-psql -d dbt2 -f order_status.sql
-psql -d dbt2 -f payment.sql
-psql -d dbt2 -f stock_level.sql
