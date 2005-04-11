c5beacb5ea2fcd6dd9da0d56c2e5832fcac1c580
fimath <>
2005-04-11 15:23:24 +0000

    [project @ 1291] Updated headers. Updated environment variable names.

--- scripts/pgsql/backup.sh
+++ scripts/pgsql/backup.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+#
+# This file is released under the terms of the Artistic License.
+# Please see the file LICENSE, included in this package, for details.
+#
+# Copyright (C) 2002 Mark Wong & Open Source Development Lab, Inc.
+#
+
+
 DIR=`dirname $0`
 . ${DIR}/pgsql_profile || exit 1
 
@@ -16,8 +24,10 @@ if [ "$OUTPUT" == "" ]; then
 	exit 1;
 fi
 
-_test=`pg_dump $DB_NAME --file=$OUTPUT | grep OK`
+_test=`pg_dump $DBNAME --file=$OUTPUT | grep OK`
 if [ "$_test" != "" ]; then
 	echo "backup failed: $_test"
 	exit 1
 fi
+
+exit 0
