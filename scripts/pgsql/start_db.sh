32932ae74976bce04e285a27b8b5aff3e0837115
markwkm <>
2006-09-29 02:19:17 +0000

    [project @ 1475] Since PostgreSQL 8.1 autovacuum, it's not longer an external program.  Removed it from the script.  Also added a flag to set the output directory of the database log file.

--- scripts/pgsql/start_db.sh
+++ scripts/pgsql/start_db.sh
@@ -13,14 +13,17 @@ DIR=`dirname ${0}`
 . ${DIR}/pgsql_profile || exit 1
 
 LOGFILE="log"
-USE_PG_AUTOVACUUM=0
-while getopts "afp:" OPT; do
+OUTDIR="."
+while getopts "afo:p:" OPT; do
 	case ${OPT} in
 	a)
 		USE_PG_AUTOVACUUM=1
 		;;
 	f)
 		rm -f ${PGDATA}/postmaster.pid
+		;;
+	o)
+		OUTDIR=${OPTARG}
 		;;
 	p)
 		PARAMETERS=$OPTARG
@@ -33,28 +36,15 @@ if [ -f ${PGDATA}/postmaster.pid ]; then
 	exit 0
 fi
 
-# We need the sleeps just in case we start pg_ctl commands too closely
-# together.  Only start pg_autovacuum if explicitly called.
-
 sleep 1
 
 if [ "${PARAMETERS}" = "" ]; then
-	${PG_CTL} -D ${PGDATA} -l ${LOGFILE} start
+	${PG_CTL} -D ${PGDATA} -l ${OUTDIR}/${LOGFILE} start
 else
-	${PG_CTL} -D ${PGDATA} -o "${PARAMETERS}" -l ${LOGFILE} start
+	${PG_CTL} -D ${PGDATA} -o "${PARAMETERS}" -l ${OUTDIR}/${LOGFILE} start
 fi
 
 sleep 10
-
-if [ ${USE_PG_AUTOVACUUM} -eq 1 ]; then
-	if [ -z ${PG_AUTOVACUUM} ]; then
-		echo "pg_autovacuum is not installed, but this is ok if you are using 8.1"
-	else
-		echo Waiting for database to start before starting pg_autovacuum...
-		sleep 10
-		${PG_AUTOVACUUM} -D
-	fi
-fi
 
 if [ ! -f ${PGDATA}/postmaster.pid ]; then
 	echo "database did not start correctly, check database log"
