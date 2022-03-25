f3481254d0732df916dbbb259eaa9c5d970e9a33
Mark Wong <markwkm@gmail.com>
2022-03-25 08:58:06 -0700

    Separate some driver functions into common file
    In anticipation of experimenting with other driver implementations, such
    as a different parallel processing model, or merging the driver and
    client code together, pull out some functions that are likely to be
    shared into a common source file.

--- src/driver.c
+++ src/driver.c
@@ -41,16 +41,12 @@ void *terminal_worker(void *data);
 
 /* Global Variables */
 pthread_t** g_tid = NULL;
-struct transaction_mix_t transaction_mix;
-struct key_time_t key_time;
-struct think_time_t think_time;
+extern int client_port;
 char hostname[32];
-int client_port = CLIENT_PORT;
 int duration = 0;
 int stop_time = 0;
 int w_id_min = 0, w_id_max = 0;
 int terminals_limit = 0;
-int terminals_per_warehouse = 0;
 int mode_altered = 0;
 int client_conn_sleep = 1000; /* milliseconds */
 int spread = 1;
@@ -76,49 +72,6 @@ pthread_mutex_t mutex_terminal_state[3][TRANSACTION_MAX] = {
 		PTHREAD_MUTEX_INITIALIZER, PTHREAD_MUTEX_INITIALIZER,
 		PTHREAD_MUTEX_INITIALIZER }
 };
-
-int create_pid_file()
-{
-	FILE * fpid;
-	char pid_filename[512];
-
-	sprintf(pid_filename, "%s/%s", output_path, DRIVER_PID_FILENAME);
-
-	fpid = fopen(pid_filename,"w");
-	if (!fpid) {
-		printf("cann't create pid file: %s\n", pid_filename);
-		return ERROR;
-	}
-
-	fprintf(fpid,"%d", (unsigned int) getpid());
-	fclose(fpid);
-
-	return OK;
-}
-
-int init_driver()
-{
-	terminals_per_warehouse = table_cardinality.districts;
-
-	transaction_mix.delivery_actual = MIX_DELIVERY;
-	transaction_mix.order_status_actual = MIX_ORDER_STATUS;
-	transaction_mix.payment_actual = MIX_PAYMENT;
-	transaction_mix.stock_level_actual = MIX_STOCK_LEVEL;
-
-	key_time.delivery = KEY_TIME_DELIVERY;
-	key_time.new_order = KEY_TIME_NEW_ORDER;
-	key_time.order_status = KEY_TIME_ORDER_STATUS;
-	key_time.payment = KEY_TIME_PAYMENT;
-	key_time.stock_level = KEY_TIME_STOCK_LEVEL;
-
-	think_time.delivery = THINK_TIME_DELIVERY;
-	think_time.new_order = THINK_TIME_NEW_ORDER;
-	think_time.order_status = THINK_TIME_ORDER_STATUS;
-	think_time.payment = THINK_TIME_PAYMENT;
-	think_time.stock_level = THINK_TIME_STOCK_LEVEL;
-
-	return OK;
-}
 
 int init_driver_logging()
 {
@@ -174,118 +127,6 @@ int integrity_terminal_worker()
 	close(sockfd);
 
 	return client_data.status;
-}
-
-int recalculate_mix()
-{
-	/*
-	 * Calculate the actual percentage that the New-Order transaction will
-	 * be execute.
-	 */
-	transaction_mix.new_order_actual = 1.0 -
-			(transaction_mix.delivery_actual +
-			transaction_mix.order_status_actual +
-			transaction_mix.payment_actual +
-			transaction_mix.stock_level_actual);
-
-	if (transaction_mix.new_order_actual < 0.0) {
-		LOG_ERROR_MESSAGE(
-				"invalid transaction mix. d %0.1f. o %0.1f. p %0.1f. s %0.1f. n %0.1f.\n",
-				transaction_mix.delivery_actual,
-				transaction_mix.order_status_actual,
-				transaction_mix.payment_actual,
-				transaction_mix.stock_level_actual,
-				transaction_mix.new_order_actual);
-		return ERROR;
-	}
-
-	/* Calculate the thresholds of each transaction. */
-	transaction_mix.new_order_threshold = transaction_mix.new_order_actual;
-	transaction_mix.payment_threshold =
-			transaction_mix.new_order_threshold +
-			transaction_mix.payment_actual;
-	transaction_mix.order_status_threshold =
-			transaction_mix.payment_threshold
-			+ transaction_mix.order_status_actual;
-	transaction_mix.delivery_threshold =
-			transaction_mix.order_status_threshold
-			+ transaction_mix.delivery_actual;
-	transaction_mix.stock_level_threshold =
-			transaction_mix.delivery_threshold +
-			transaction_mix.stock_level_actual;
-
-	return OK;
-}
-
-int set_client_hostname(char *addr)
-{
-	strcpy(hostname, addr);
-	printf("connecting to client at '%s'\n", hostname);
-	fflush(stdout);
-	return OK;
-}
-
-int set_client_port(int port)
-{
-	client_port = port;
-	printf("connecting to client port at '%d'\n", client_port);
-	fflush(stdout);
-	return OK;
-}
-
-int set_duration(int seconds)
-{
-	duration = seconds;
-	return OK;
-}
-
-int set_table_cardinality(int table, int cardinality)
-{
-	switch (table) {
-	case TABLE_WAREHOUSE:
-		table_cardinality.warehouses = cardinality;
-		break;
-	case TABLE_CUSTOMER:
-		table_cardinality.customers = cardinality;
-		break;
-	case TABLE_ITEM:
-		table_cardinality.items = cardinality;
-		break;
-	case TABLE_ORDER:
-		table_cardinality.orders = cardinality;
-		break;
-	case TABLE_NEW_ORDER:
-		table_cardinality.new_orders = cardinality;
-		break;
-	default:
-		return ERROR;
-	}
-
-	return OK;
-}
-
-int set_transaction_mix(int transaction, double mix)
-{
-	switch (transaction) {
-	case DELIVERY:
-		transaction_mix.delivery_actual = mix;
-		break;
-	case NEW_ORDER:
-		transaction_mix.new_order_actual = mix;
-		break;
-	case ORDER_STATUS:
-		transaction_mix.order_status_actual = mix;
-		break;
-	case PAYMENT:
-		transaction_mix.payment_actual = mix;
-		break;
-	case STOCK_LEVEL:
-		transaction_mix.stock_level_actual = mix;
-		break;
-	default:
-		return ERROR;
-	}
-	return OK;
 }
 
 int start_driver()
