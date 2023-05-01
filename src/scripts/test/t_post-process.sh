4615ca49d4d6c2ac235f1996bd6489ac9d8c6961
Mark Wong <markwkm@gmail.com>
2023-05-01 13:23:41 -0700

    Rename post-process script to "report" script
    Shorten merge the post-process script to handle both the transaction
    rate calculation and creating an HTML report.
    
    Also add a man page for the report script.

--- src/scripts/test/t_post-process.sh
+++ src/scripts/test/t_post-process.sh
@@ -3,24 +3,25 @@
 oneTimeSetUp() {
 	THISDIR=$(dirname "$0")
 	TOPDIR="${THISDIR}/../../.."
-	POSTPROCESS="${TOPDIR}/src/scripts/dbt2-post-process"
 	ACTUALOUTPUT="${SHUNIT_TMPDIR}/summary.rst"
 	EXPECTEDOUTPUT="${TOPDIR}/src/scripts/test/summary.rst.expected"
 	export PATH="${TOPDIR}/builds/debug:${TOPDIR}/src/scripts:${PATH}"
 }
 
 testPostProcessR() {
-	export DBT2LANG="R"
 	# shellcheck disable=SC2086
-	$POSTPROCESS ${TOPDIR}/src/scripts/test/mix-*.log > "$ACTUALOUTPUT"
+	find ${TOPDIR}/src/scripts/test -name "mix-*.log" -print0 | \
+			xargs -0 ${TOPDIR}/src/scripts/dbt2-post-process.r \
+			> "$ACTUALOUTPUT"
 	diff "$ACTUALOUTPUT" "$EXPECTEDOUTPUT"
 	assertEquals "match" 0 $?
 }
 
 testPostProcessSQLite() {
-	export DBT2LANG="sqlite3"
 	# shellcheck disable=SC2086
-	$POSTPROCESS ${TOPDIR}/src/scripts/test/mix-*.log > "$ACTUALOUTPUT"
+	find ${TOPDIR}/src/scripts/test -name "mix-*.log" -print0 | \
+			xargs -0 ${TOPDIR}/builds/debug/dbt2-post-process.sql \
+			> "$ACTUALOUTPUT"
 	diff "$ACTUALOUTPUT" "$EXPECTEDOUTPUT"
 	assertEquals "match" 0 $?
 }
